2970fe77914e76dab21927541811b6b7b4c24647
isa <159180446+Isagapito@users.noreply.github.com>
2024-10-31 10:59:57 -0300

    Add files via upload

--- Atividade012/A12-LojaDB.sql
+++ Atividade012/A12-LojaDB.sql
@@ -40,4 +40,80 @@ INSERT INTO tb_vendas (id_NF, id_Item, Cod_Produto,valor_Unit, quantidade, Desco
 SELECT * FROM tb_vendas ; 
 
 /* ATIVIDADE 1*/
+SELECT id_NF, id_Item, Cod_Produto, Valor_Unit FROM tb_vendas
+WHERE Desconto  IS NULL ; 
 
+/* ATIVIDADE 2*/ 
+SELECT id_NF, id_Item, Cod_Produto, Valor_Unit, Valor_Unit - (Valor_Unit*(Desconto/100)) as "Valor_Vendido"
+FROM tb_vendas 
+WHERE Desconto > 0; 
+
+/* ATIVIDADE 3 */ 
+UPDATE tb_vendas 
+SET Desconto = 0 
+WHERE Desconto IS NULL ; 
+
+SELECT * FROM tb_vendas ; 
+
+/* ATIVIDA 4 */ 
+SELECT id_NF, id_Item,Cod_Produto, Valor_Unit * quantidade as valor_total, Desconto, valor_Unit-(Valor_Unit*(Desconto/100)) as valor_vendido FROM tb_vendas ; 
+
+/* ATIVIDADE 5*/ 
+SELECT id_NF , 
+Sum(quantidade*valor_Unit)
+as valor_total 
+FROM tb_vendas 
+GROUP BY id_NF
+ORDER BY id_NF DESC; 
+
+/*ATIVIDADE 6 */ 
+SELECT id_NF , 
+Valor_Unit - (Valor_Unit*(Desconto/100)) 
+AS"valor_vendido" , 
+SUM(quantidade*valor_Unit)
+as "valor_total" 
+FROM tb_vendas 
+GROUP BY id_NF
+ORDER BY id_NF DESC;  
+
+/*ATIVIDADE 7 */ 
+SELECT Cod_Produto, quantidade 
+ORDER BY Cod_Produto ; 
+
+/* ATIVIDADE 8 */ 
+SELECT id_NF, Cod_Produto,
+COUNT(quantidade)
+FROM tb_vendas
+WHERE quantidade > 10
+GROUP BY id_NF, Cod_Produto;
+
+/*ATIVIDADE 9 */ 
+SELECT id_NF AS valor_total FROM tb_vendas
+WHERE id_NF > 500
+GROUP BY id_NF, valor_total
+ORDER BY id_NF DESC; 
+
+/*ATIVIDADE 10 */ 
+SELECT Cod_Produto, 
+AVG  (Desconto)
+as media 
+FROM tb_vendas 
+GROUP BY Cod_Produto ; 
+
+/*ATIVIDADE 11 */ 
+SELECT Cod_Produto, 
+AVG  (Desconto)
+as media ,
+AVG (min) 
+as Menor , 
+AVG (max) 
+as Maior 
+FROM tb_vendas 
+GROUP BY  Menor ;
+
+/*ATIVIDADE 12 */ 
+SELECT id_NF ,
+count (id_NF) as quantidade 
+FROM tb_vendas 
+WHERE quantidade 
+GROUP BY id_NF;
